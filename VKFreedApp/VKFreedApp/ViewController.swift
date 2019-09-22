3f19c08f78cebaa44f9e2b70dab6f15f2b69d368
sergeev <v.sergeev.m@gmail.com>
2019-09-23 02:55:21 +0700

    init app

---
+++ VKFreedApp/VKFreedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFreedApp
+//
+//  Created by V.Sergeev on 23/09/2019.
+//  Copyright © 2019 v.sergeev.m@icloud.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
